d079d439441a3e75829abe864d5fd66c1f84bdfa
Xtuber14 <72020804+Xtuber14@users.noreply.github.com>
2023-08-28 19:05:21 +0200

    First Commit
    This is the first commit of this project

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+import time
+version = 'v1.0\n'
+output_text = ''
+welcome = 'Welcome to FandomHTML ' + version
+initial_message = '1. Add <br>\n2. Add <br> from a file .txt\nPress X to exit'
+
+def convert_newlines_to_br(input_text):
+    return input_text.replace('\n', '<br>')
+
+# Start Program
+print(welcome + initial_message)
+init_welcome = input()
+
+if init_welcome == '1':
+    print("Enter your text (type 'X' on a new line to exit input):")
+    input_lines = []
+
+    while True:
+        line = input()
+        if line == 'X':
+            break
+        input_lines.append(line)
+
+    input_text = '\n'.join(input_lines)
+
+    # Convert and print the modified text
+    output_text = convert_newlines_to_br(input_text)
+    print(output_text)
+    time.sleep(30)
