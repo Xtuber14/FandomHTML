035666d8a7f8cc00c1f4ba10da0495559ebd5ba0
Xtuber14 <72020804+Xtuber14@users.noreply.github.com>
2023-08-29 14:37:55 +0200

    Optimized Code + Improved Readability

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import sys
 import time
-version = 'v1.0\n'
-output_text = ''
-welcome = 'Welcome to FandomHTML ' + version
-initial_message = '1. Add <br>\n2. Add <br> from a file .txt (WIP)\nPress X to exit'
+import pyperclip
+
+VERSION = 'v1.2'
+WELCOME_MESSAGE = f'Welcome to FandomHTML {VERSION}'
+INITIAL_OPTIONS = (
+    "1. Add <br>\n2. Add <br> from a file .txt\nPress X to exit"
+)
 
 def convert_newlines_to_br(input_text):
     return input_text.replace('\n', '<br>')
 
-# Start Program
-print(welcome + initial_message)
-init_welcome = input()
+def convert_newlines_to_brn(input_text):
+    return input_text.replace('\n', '<br>\n')
 
-if init_welcome == '1':
+def get_input_lines():
     print("Enter your text (type 'X' on a new line to exit input):")
     input_lines = []
 
@@ -22,11 +24,46 @@ if init_welcome == '1':
             break
         input_lines.append(line)
 
-    input_text = '\n'.join(input_lines)
+    return '\n'.join(input_lines)
+
+def process_option_1():
+    input_text = get_input_lines()
 
     # Convert and print the modified text
     output_text = convert_newlines_to_br(input_text)
     print(output_text)
-    time.sleep(30)
-if init_welcome == 'X':
-    sys.exit()
+    pyperclip.copy(output_text)
+    print('Text copied to clipboard')
+    time.sleep(5)
+
+def process_option_2():
+    filepath = input('Paste here the path for the file: ')
+    filepath = filepath.replace('"', "")
+    
+    try:
+        with open(filepath, "r") as file:
+            content = file.read()
+
+        output_text = convert_newlines_to_brn(content)
+
+        with open(filepath, "a") as file:
+            file.write("\n\n" + output_text)
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+def main():
+    print(WELCOME_MESSAGE)
+    print(INITIAL_OPTIONS)
+    init_welcome = input()
+
+    if init_welcome == '1':
+        process_option_1()
+    elif init_welcome == '2':
+        process_option_2()
+    elif init_welcome == 'X':
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
